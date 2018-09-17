233ab7f17c3524fdbcfafcb588f3934a9655e24b
SanderLB <sanderlb@stud.ntnu.no>
2018-09-17 13:44:44 +0200

    Testing google analytics

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125913159-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+  		gtag('config', 'UA-125913159-1');
+	</script>
+
+</head>
+
 <html>
         <img src="img/Email_symbol.png">
         Hello there, General Kenobi
