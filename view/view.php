3ae51da92b3f3fee5ba4bbda5d57bcdf58c4883e
unknown <sanderlb@stud.ntnu.no>
2018-09-18 23:37:25 +0200

    Added header

--- view/view.php
+++ view/view.php
@@ -1,4 +1,11 @@
 <div id="wrapper">
-    <h1>"HTML is hard" - Tobias Moe</h1>
+    <div id="header">
+        <img src="img/white_logo_transparent">
+        <h1>"HTML is hard" - Tobias Moe</h1>
+        <div id="login">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password">
+        </div>
+    </div>
 </div>	
 
