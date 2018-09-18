835791a0312da2089a04e2f7fca29b9598a830bd
tobiasmo <tobiasmo@stud.ntnu.no>
2018-09-18 15:36:01 +0200

    modifed files to view

--- view/view.php
+++ view/view.php
@@ -1,29 +1,23 @@
 <?php
-#include_once("view/forumview.php");
-
-	abstract Class View {
-		public function create() {
-			echo <<<HTML
+public function create() {
+	echo <<<HTML
 <!DOCTYPE html>
 <html>
 <head>
-<title>
-HTML;
-	echo "Title";
-	echo <<<HTML
-</title>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125913159-1"></script>
+        <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-125913159-1');
+        </script>
+
 </head>
-
-<body>
-<h1>
-HTML;
-	echo "ECHOOO";
-	echo <<<<HTML
-</h1>
-</body>
+   	<img src="img/Email_symbol.png">
+        Hello there, General Kenobi
 </html>
-HTML;
-		}
-	}
 
+HTML;
 ?>
