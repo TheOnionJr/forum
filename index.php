f4b6d3bb9f7459ce97e2511ddf9eee93b06dfd9f
SanderLB <sanderlb@stud.ntnu.no>
2018-09-18 19:34:40 +0200

    commit die

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
-
-<head>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125913159-1"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-  		gtag('config', 'UA-125913159-1');
-	</script>
-
-</head>
 <html>
-	<?php
-		include_once("controller/Controller.php");
-		$controller = new controller();
-		$controller->invoke();
-	?>
+	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125913159-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+  			gtag('config', 'UA-125913159-1');
+		</script>
+
+	</head>
+	<body>
+		<?php
+			include_once("controller/Controller.php");
+			$controller = new controller();
+			$controller->invoke();
+		?>
+	</body>
 </html>
