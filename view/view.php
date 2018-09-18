ab037bdbccf5d259f0d352f400ea6f70c24729c1
unknown <sanderlb@stud.ntnu.no>
2018-09-18 23:48:37 +0200

    Design

--- view/view.php
+++ view/view.php
@@ -1,6 +1,6 @@
 <div id="wrapper">
     <div id="header">
-        <img src="img/white_logo_transparent.png" width="20%" height="20%">
+        <img src="img/white_logo_transparent.png" width="20%" height="20%" align="left">
         <div id="login">
             Username: <input type="text" name="username"><br>
             Password: <input type="password" name="password">
