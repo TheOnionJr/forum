1773d3944678d7201823dbc5881e6130a2c0a3ea
unknown <sanderlb@stud.ntnu.no>
2018-09-19 00:57:12 +0200

    Home link

--- view/view.php
+++ view/view.php
@@ -1,6 +1,6 @@
 <div id="wrapper">
     <div id="header">
-        <img src="img/white_logo_transparent.png" align="left">
+        <a href="index.php"><img src="img/white_logo_transparent.png" align="left" ></a>
         <div id="login">
             Username: <input type="text" name="username"><br>
             Password: <input type="password" name="password">
