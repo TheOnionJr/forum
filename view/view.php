ac2cac245117e26b9b15425228c64edcdc811fe2
unknown <sanderlb@stud.ntnu.no>
2018-09-18 23:52:21 +0200

    Design

--- view/view.php
+++ view/view.php
@@ -1,6 +1,6 @@
 <div id="wrapper">
     <div id="header">
-        <img src="img/white_logo_transparent.png">
+        <img src="img/white_logo_transparent.png" align="left">
         <div id="login">
             Username: <input type="text" name="username"><br>
             Password: <input type="password" name="password">
