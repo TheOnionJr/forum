4247be532dffac1e770561d418468de01948c037
unknown <sanderlb@stud.ntnu.no>
2018-09-18 23:39:44 +0200

    Added header

--- view/view.php
+++ view/view.php
@@ -1,6 +1,6 @@
 <div id="wrapper">
     <div id="header">
-        <img src="img/white_logo_transparent.png">
+        <img src="img/white_logo_transparent.png" width="25%" height="10%">
         <div id="login">
             Username: <input type="text" name="username"><br>
             Password: <input type="password" name="password">
