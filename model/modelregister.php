7d0bd403c2e72215c81518fa526c74b9ccfdc9da
tobiasmo <tobiasmo@stud.ntnu.no>
2018-09-19 21:54:13 +0200

    MERGE ME BITCH

--- model/modelregister.php
+++ model/modelregister.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start();
+//session_start();
 
 $username = "";
 $email = "";
@@ -10,10 +10,29 @@ $db = mysqli_connect("localhost", "guest", "", "forum");
 
 
 if (isset($_POST['reg_user'])) {
-	$username = mysqli_real_escape_string($db, $_POST['username']);
+	
+	/*
+	$username = mysqli_real_escape_string($db, $_POST['username']);	
 	$email = mysqli_real_escape_string($db, $_POST['email']);
 	$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 	$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+	*/
+	$username = filter_var($_POST['username'], FILTER_SANITIZE_EMAIL);
+	if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+		array_push($errors, "Not a valid username!");
+	}
+	$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		array_push($errors, "Not a valid email!");
+	}
+	$password_1 = filter_var($_POST['password_1'], FILTER_SANITIZE_EMAIL);
+	if (!filter_var($password_1, FILTER_VALIDATE_EMAIL)) {
+		array_push($errors, "Not a valid password!");
+	}
+	$password_2 = filter_var($_POST['password_2'], FILTER_SANITIZE_EMAIL);
+	if (!filter_var($password_2, FILTER_VALIDATE_EMAIL)) {
+		array_push($errors, "Not a valid password!");
+	}
 
 	if (empty($username)) { array_push($errors, "Username is required"); }
 	if (empty($email)) { array_push($errors, "Email is required"); }
@@ -23,13 +42,19 @@ if (isset($_POST['reg_user'])) {
 	}
 
 
-	$user_check_query = "SELECT * FROM uUser WHERE uUsername='$username'";
+	$user_check_query = "SELECT * FROM uUser WHERE uUsername='$username' OR email='$email' LIMIT 1";
 	$result = mysqli_query($db, $user_check_query);
 	$user = mysqli_fetch_assoc($result);
 
 	if ($user) { 
-		array_push($errors, "Username already exists");
+		if ($user['username'] === $username) {
+			array_push($errors, "Username already exists");
+		}
+		if ($user['email'] === $email) {
+      		array_push($errors, "Email already exists");
+    	}	
 	}
+
 
 	if (count($errors) == 0) {
 		$password = md5($password_1);
