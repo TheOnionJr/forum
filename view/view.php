842418284c7ee9ebe90f75c5b369d5436ac94d57
unknown <sanderlb@stud.ntnu.no>
2018-09-19 15:45:16 +0200

    Added subforum display php code

--- view/view.php
+++ view/view.php
@@ -6,5 +6,35 @@
             Password: <input type="password" name="password">
         </div>
     </div>
+    <table>
+    	<?php
+			$con=mysqli_connect("localhost","guest","","forum");
+			// Check connection
+			if (mysqli_connect_errno()){
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			}
+
+			$result = mysqli_query($con,"SELECT * FROM subforums");
+
+			echo "<table border='1'>
+			<tr>
+				<th>Subforum</th>
+				<th>Topics</th>
+				<th>Posts</th>
+			</tr>";
+
+			while($row = mysqli_fetch_array($result))
+			{
+				echo "<tr>";
+				echo "<td>" . $row['sName'] . "</td>";
+				echo "<td>" . $row['sNumTopics'] . "</td>";
+				echo "<td>" . $row['sNumPosts'] . "</td>";
+				echo "</tr>";
+			}	
+			echo "</table>";
+
+			mysqli_close($con);
+		?>
+    </table>
 </div>	
 
