ff2508d6f076ff6fa86d1a1f111666aafe2d8117
tobiasmo <tobiasmo@stud.ntnu.no>
2018-09-19 16:23:20 +0200

    fix

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-
-		<?php
-			include_once("controller/controller.php");
-			$controller = new controller();
-			$controller->invoke();
-		?>
+<?php
+	include_once("controller/controller.php");
+	$controller = new controller();
+	$controller->invoke();
+?>
