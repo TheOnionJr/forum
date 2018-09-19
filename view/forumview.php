228b2f451e51749fff4f171301e84aebad841a12
tobiasmo <tobiasmo@stud.ntnu.no>
2018-09-19 16:09:58 +0200

    moved a table from view.php to forumview.php

--- view/forumview.php
+++ view/forumview.php
@@ -2,3 +2,38 @@
 include_once('view.php');
 
 ?>
+
+
+<table>
+	<?php
+		$con=mysqli_connect("localhost","user","","forum");
+		// Check connection
+		if (mysqli_connect_errno()){
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+
+		$result = mysqli_query($con,"SELECT * FROM subforums");
+
+		echo "<table border='1'>
+		<tr>
+			<th>Subforum</th>
+			<th>Topics</th>
+			<th>Posts</th>
+		</tr>";
+
+		while($row = mysqli_fetch_array($result))
+		{
+			echo "<tr>";
+			echo "<td>" . $row['sName'] . "</td>";
+			echo "<td>" . $row['sNumTopic'] . "</td>";
+			echo "<td>" . $row['sNumPosts'] . "</td>";
+			echo "</tr>";
+		}	
+		echo "</table>";
+
+		mysqli_close($con);
+	?>
+</table>
+
+
+
