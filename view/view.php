66984ff047fbf201f09cb0fc2075066b84043776
unknown <sanderlb@stud.ntnu.no>
2018-09-19 16:18:12 +0200

    Format

--- view/view.php
+++ view/view.php
@@ -1,12 +1,10 @@
-<div id="wrapper">
-    <div id="header">
-        <a href="index.php"><img src="img/white_logo_transparent.png" align="left" ></a>
-        <div id="login">
-            Username: <input type="text" name="username"><br>
-            Password: <input type="password" name="password"><br>
-            <a href="view/registerview.php" id="register">Register</a>		
-        </div>
+<div id="header">
+    <a href="index.php"><img src="img/white_logo_transparent.png" align="left" ></a>
+    <div id="login">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <a href="view/registerview.php" id="register">Register</a>		
     </div>
-    
-</div>	
+</div>
+	
 
