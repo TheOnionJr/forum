4807972883cb6846577ad6c762ebed9c1e0fc2d5
unknown <sanderlb@stud.ntnu.no>
2018-09-22 14:04:20 +0200

    Fjernet google analytics. Den generer alt for mange false positives under analyse

--- view/view.php
+++ view/view.php
@@ -1,4 +1,5 @@
 <?php 
+
 	$path = $_SERVER['DOCUMENT_ROOT']; 	//Find the document root
 	$path .= "/model/modellogin.php"; 	//Set absolute path
 	include($path);
@@ -12,14 +13,9 @@
 ?>
 <html>
 	<head>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125913159-1"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-				gtag('config', 'UA-125913159-1');
-		</script>
+		<?php
+			header("X-XSS-Protection: 1; mode=block"); //Browser XXS protection
+		?>
 		<link type="text/css" id="dark-mode" rel="stylesheet" href="/css/darkmode.css">
 	</head>
 	<body>
