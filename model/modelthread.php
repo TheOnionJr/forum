f0f53a5d8c35af1c73b6a5920f0091eb9d4b99e8
Aproydtix <eirikhiishauge@hotmail.com>
2018-09-28 18:55:05 +0200

    Slight update to threadview, added comments

--- model/modelthread.php
+++ model/modelthread.php
@@ -17,29 +17,44 @@
 		echo "This thread does not exist";
 	}
 	//Else statement not necessary, if $result = 0 -> nothing will be printed
+
+	
 	while($row = mysqli_fetch_array($result))
 	{
+		//	Thread name
 		echo "<table>";
 		$thID = htmlentities($row['thID'], ENT_QUOTES, 'UTF-8');
-		echo "<tr>";
-		echo "<th>" . htmlentities($row['thName'], ENT_QUOTES, 'UTF-8') . "</th>";
-		echo "</tr>";
+		echo "<tr><th>" . htmlentities($row['thName'], ENT_QUOTES, 'UTF-8') . "</th></tr>";
+
+		//	Load and populate posts
 		$posts = mysqli_query($con,"SELECT * FROM posts WHERE pThreadID = $thID ORDER BY pTimestamp");
 		while($post_row = mysqli_fetch_array($posts)) {
-			echo "<th>" . htmlentities($post_row['pAuthor'], ENT_QUOTES, 'UTF-8') . " | " . htmlentities($post_row['pTimestamp'], ENT_QUOTES, 'UTF-8') . "</th>";
+			echo "<th>";
+
+			// Username and Timestamp
+			if (false)	//	admin or mod
+				echo '<font color="gold">';	//	gold for moderators, darkorange for admins
+			echo htmlentities($post_row['pAuthor'], ENT_QUOTES, 'UTF-8');
+			if (false)	//	admin or mod
+				echo '</font>';
+			echo " | " . htmlentities($post_row['pTimestamp'], ENT_QUOTES, 'UTF-8') . "</th>";
+
+			//	Post content
 			echo "<tr>";
 			$pID = $post_row['pID'];
-			if ($post_row['pDeleted'])
+			if ($post_row['pDeleted'])	//	Deleted post
 				echo "<td>" . '<font color="red">This post was deleted by ' . htmlentities($post_row['pDeletedBy'], ENT_QUOTES, 'UTF-8') . ".</font></td>";
-			else
+			else						//	Post content
 				echo "<td>" . htmlentities($post_row['pContent'], ENT_QUOTES, 'UTF-8') . "</td>";
 			echo "</tr>";
+
+			//	Reply, Edit, Delete functions
 			if (!$post_row['pDeleted'])
 			{
 				echo "<tr><td>";
-				echo "Reply";
-				echo "| Edit";
-				echo "| Delete";
+				echo "Reply";				//	Replace these with functions
+				echo " | " . "Edit";		//	Replace these with functions
+				echo " | " . "Delete";		//	Replace these with functions
 				echo "</td></tr>";
 			}
 		}
