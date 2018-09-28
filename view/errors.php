51b378fa235a899711513f551d5987a2a68e23fe
unknown <sanderlb@stud.ntnu.no>
2018-09-28 19:55:07 +0200

    Login error is now JS confirm warning, dose'nt fuck formatting

--- view/errors.php
+++ view/errors.php
@@ -1,7 +1,12 @@
 <?php  if (count($errors) > 0) : ?>
   <div class="error">
-  	<?php foreach ($errors as $error) : ?>
-  	  <p><?php echo $error ?></p>
-  	<?php endforeach ?>
+  	<?php echo "
+  	  	<script type=\"text/javascript\">
+  	  			window.confirm(\"" ?>
+  		<?php foreach ($errors as $error) : ?>
+  	  		<?php echo filter_var($error, FILTER_SANITIZE_STRING)?>
+  	  	<?php endforeach ?>
+  	  <?php echo "\")</script>" ?>
+  	
   </div>
 <?php  endif ?>
