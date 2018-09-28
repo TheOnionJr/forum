8eca65039d7e215ae29a18de73e8509dd27b1b88
SanderLB <sanderlb@stud.ntnu.no>
2018-09-28 11:45:25 +0200

    Commenting

--- model/modelpost.php
+++ model/modelpost.php
@@ -13,26 +13,26 @@ $replyTo=htmlentities($_POST['replyTo'], ENT_QUOTES, 'UTF-8');				//Get id of po
 
 //Verifying everything about the post:
 function canPost(username, thread, topic, subforum, replyTo, connection) {
-	$con=connection;
-	$stmt = $con->prepare("SELECT threads.thLock, threads.thMoved, threads.thID, topics.tID, subforums.sID FROM topics INNER JOIN threads ON threads.thTopicID = topics.tID INNER JOIN subforums ON subforums.sID = topics.tSubForumID WHERE thID = ?"); //Fuuuuck me this query
-	$stmt->bind_param('i', thread);
-	$stmt->execute();
-	$idverify = $stmt->get_result();
-	$stmt = $con->prepare("SELECT uUsername FROM uuser WHERE uUsername = ?");
-	$stmt->bind_param('s', username);
-	$stmt->execute();
-	$usr = $stmt->get_result();
+	$con=connection;																				//Using the sendt connection
+	$stmt = $con->prepare("SELECT threads.thLock, threads.thMoved, threads.thID, topics.tID, subforums.sID FROM topics INNER JOIN threads ON threads.thTopicID = topics.tID INNER JOIN subforums ON subforums.sID = topics.tSubForumID WHERE thID = ?"); 						   //Fuuuuck me this query
+	$stmt->bind_param('i', thread);																	//Binding parameter type
+	$stmt->execute();																				//Execute query
+	$idverify = $stmt->get_result();																//Store result
+	$stmt = $con->prepare("SELECT uUsername FROM uuser WHERE uUsername = ?");						//Select username parameter
+	$stmt->bind_param('s', username);																//Bind parameter type
+	$stmt->execute();																				//Execute query
+	$usr = $stmt->get_result();																		//Store result
 	if (!mysqli_num_rows($usr) or !mysqli_numrows($idverify)){										//Checks if thread and username even exsist.
-		return false;
+		return false;																				//Return can not post
 	}
 	if (username != filter_var($_SESSION['username'], FILTER_SANITIZE_STRING)){						//Checks if username parameter is the same as the session username. Prevents spoofing.
-		return false;
+		return false;																				//Return can not post
 	}
 	if ($idverify['thID'] != thread or $idverify['tID'] != topic or $idverify['sID'] != subforum) {	//Checks that the sendt parameters are correct
-		return false;																				//I cut this into two IF test to not go insane
-	}
+		return false;																				//Return can not post
+	}																								//I cut this into two IF test to not go insane
 	if ($idverify['thLock'] == 1 or $idverify['thMoved'] == 1){										//Checks if the thread is locked or moved.
-		return false;
+		return false;																				//Return can not post
 	}
 	if (replyTo != NULL) {																			//Checks if this is a reply
 		$stmt = $con->prepare("SELECT pID, pDeleted FROM posts WHERE pID = ?");						//Querys info about original post
@@ -40,7 +40,7 @@ function canPost(username, thread, topic, subforum, replyTo, connection) {
 		$stmt->execute();																			//Executing statement
 		$op=$stmt->get_result();																	//Storing result
 		if (replyTo != $op['pID'] and $op['pDeleted'] != 0) {										//Checks if replying to post that exist and is not deleted
-			return false;																			//If not, return false
+			return false;																			//If not, return false. Can not post
 		}
 	}
 }
