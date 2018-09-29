267876cdcf04cd27275002320640697f7404da49
unknown <sanderlb@stud.ntnu.no>
2018-09-29 13:46:51 +0200

    Fix

--- view/errors.php
+++ view/errors.php
@@ -1,13 +1,11 @@
-<?php  if (count($errors) > 0) : ?>										<!-- If error -->
+<?php  if (count($errors) > 0) : ?>							
   <div class="error">
-  	<!-- Start javascript -->
   	<?php echo "
   	<script type=\"text/javascript\">
-  	  	window.confirm(\"" ?>
-  			<?php foreach ($errors as $error) : ?> 						<!-- Print all errors -->
-  	  		<?php echo filter_var($error, FILTER_SANITIZE_STRING)?>		<!-- Actuall printing and filtering -->
-  	  		<?php endforeach ?>											<!-- This needs some improvement -->
+  	window.confirm(\"" ?>
+  			<?php foreach ($errors as $error) : ?>
+  	  		<?php echo filter_var($error, FILTER_SANITIZE_STRING)?>
+  	  		<?php endforeach ?>
   	<?php echo "\")</script>" ?>
-  	<!-- End javascript -->
   </div>
 <?php  endif ?>
