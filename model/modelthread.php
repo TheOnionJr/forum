97c47f588bacaf466bda2e82f894031654b5d011
sindre <sindrebg@stud.ntnu.no>
2018-10-09 17:56:54 +0200

    tiny bugfix

--- model/modelthread.php
+++ model/modelthread.php
@@ -34,7 +34,7 @@
 
 
 	
-	$maxPage = ceil((mysqli_fetch_array(mysqli_query($con, "SELECT COUNT(*) FROM posts WHERE pThreadID = $threadID"))['COUNT(*)'])/25);
+	$maxPage = ceil((mysqli_fetch_array(mysqli_query($con, "SELECT COUNT(*) FROM posts WHERE pThreadID = $threadID AND pReplyTo IS NULL"))['COUNT(*)'])/25);
 	
 	if($page > $maxPage || $page < 1)
 		$page = 1;
